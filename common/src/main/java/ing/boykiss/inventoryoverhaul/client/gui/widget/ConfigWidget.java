6f31e4a8dac236a9fa8bc936b2506d4d26e81ca2
mel <mcmelon@nodiumhosting.com>
2025-07-28 04:33:08 +0200

    refactor: config widget cleanup

--- common/src/main/java/ing/boykiss/inventoryoverhaul/client/gui/widget/ConfigWidget.java
+++ common/src/main/java/ing/boykiss/inventoryoverhaul/client/gui/widget/ConfigWidget.java
@@ -8,8 +8,8 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.components.StringWidget;
 import net.minecraft.network.chat.Component;
-import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
@@ -40,20 +40,22 @@ public class ConfigWidget<T extends AbstractWidget> {
                 .toList();
     }
 
-    private static @Nullable ConfigWidget<?> create(Field field, ClientConfig clientConfig) {
+    private static ConfigWidget<?> create(Field field, ClientConfig clientConfig) {
+        Class<?> type = field.getType();
+        ConfigOption configOption = field.getAnnotation(ConfigOption.class);
+
+        WidgetData widgetData = new WidgetData(field, type, configOption, clientConfig);
+
+        boolean slider = configOption.slider();
+        double sliderStep = configOption.sliderStep();
+        double min = configOption.min();
+        double max = configOption.max();
+
         try {
-            Class<?> type = field.getType();
-            ConfigOption configOption = field.getAnnotation(ConfigOption.class);
-
-            boolean slider = configOption.slider();
-            double sliderStep = configOption.sliderStep();
-            double min = configOption.min();
-            double max = configOption.max();
-
             if (type.isEnum()) {
                 return slider
-                        ? createEnumSlider(field, type, configOption, clientConfig, sliderStep)
-                        : createEnumButton(field, type, configOption, clientConfig);
+                        ? createEnumSlider(widgetData, sliderStep)
+                        : createEnumButton(widgetData);
             }
 
             if (type == short.class || type == Short.class ||
@@ -63,60 +65,65 @@ public class ConfigWidget<T extends AbstractWidget> {
                     type == float.class || type == Float.class ||
                     type == double.class || type == Double.class) {
                 return (slider && !Double.isNaN(min) && !Double.isNaN(max))
-                        ? createNumericSlider(field, type, configOption, clientConfig, min, max, sliderStep)
-                        : createNumericInput(field, type, configOption, clientConfig, min, max);
+                        ? createNumericSlider(widgetData, min, max, sliderStep)
+                        : createNumericInput(widgetData, min, max);
             }
 
             if (type == String.class) {
-                return createStringInput(field, type, configOption, clientConfig);
+                return createStringInput(widgetData);
             }
 
             InventoryOverhaul.LOGGER.error("config widget for type {} not implemented", type.getName());
-            return null;
         } catch (IllegalAccessException e) {
-            return null;
+            InventoryOverhaul.LOGGER.error(e.getMessage());
         }
+
+        return createErrorWidget(configOption);
     }
 
-    private static ConfigWidget<Button> createEnumButton(Field field, Class<?> type, ConfigOption configOption, ClientConfig clientConfig) throws IllegalAccessException {
-        return new ConfigWidget<>(configOption.size(), Button.builder(getWidgetText(field.getName(), field.get(clientConfig).toString()), (b) -> {
+    private static ConfigWidget<Button> createEnumButton(WidgetData widgetData) throws IllegalAccessException {
+        return new ConfigWidget<>(widgetData.configOption.size(), Button.builder(getWidgetText(widgetData.field.getName(), widgetData.field.get(widgetData.clientConfig).toString()), (b) -> {
             try {
-                Object currentOption = field.get(clientConfig);
+                Object currentOption = widgetData.field.get(widgetData.clientConfig);
 
-                Object[] enumConstants = type.getEnumConstants();
+                Object[] enumConstants = widgetData.type.getEnumConstants();
                 int currentIndex = Arrays.asList(enumConstants).indexOf(currentOption);
                 int nextIndex = (currentIndex + 1) % enumConstants.length;
                 Object next = enumConstants[nextIndex];
 
-                field.set(clientConfig, next);
+                widgetData.field.set(widgetData.clientConfig, next);
 
-                clientConfig.save();
+                widgetData.clientConfig.save();
 
-                b.setMessage(getWidgetText(field.getName(), next.toString()));
+                b.setMessage(getWidgetText(widgetData.field.getName(), next.toString()));
             } catch (IllegalAccessException e) {
                 InventoryOverhaul.LOGGER.error(e.getMessage());
             }
-        }).width(configOption.size().getSize()).build());
+        }).width(widgetData.configOption.size().getSize()).build());
     }
 
-    private static ConfigWidget<Button> createEnumSlider(Field field, Class<?> type, ConfigOption configOption, ClientConfig clientConfig, double step) throws IllegalAccessException {
-        return new ConfigWidget<>(configOption.size(), Button.builder(getWidgetText(field.getName(), field.get(clientConfig).toString()), (b) -> {
+    private static ConfigWidget<Button> createEnumSlider(WidgetData widgetData, double step) throws IllegalAccessException {
+        return new ConfigWidget<>(widgetData.configOption.size(), Button.builder(getWidgetText(widgetData.field.getName(), widgetData.field.get(widgetData.clientConfig).toString()), (b) -> {
 
-        }).width(configOption.size().getSize()).build());
+        }).width(widgetData.configOption.size().getSize()).build());
     }
 
-    private static ConfigWidget<EditBox> createNumericInput(Field field, Class<?> type, ConfigOption configOption, ClientConfig clientConfig, double min, double max) throws IllegalAccessException {
-        return new ConfigWidget<>(configOption.size(), new EditBox(Minecraft.getInstance().font, configOption.size().getSize(), WIDGET_HEIGHT, getWidgetText(field.getName())));
+    private static ConfigWidget<EditBox> createNumericInput(WidgetData widgetData, double min, double max) throws IllegalAccessException {
+        return new ConfigWidget<>(widgetData.configOption.size(), new EditBox(Minecraft.getInstance().font, widgetData.configOption.size().getSize(), WIDGET_HEIGHT, getWidgetText(widgetData.field.getName())));
     }
 
-    private static ConfigWidget<Button> createNumericSlider(Field field, Class<?> type, ConfigOption configOption, ClientConfig clientConfig, double min, double max, double step) throws IllegalAccessException {
-        return new ConfigWidget<>(configOption.size(), Button.builder(getWidgetText(field.getName(), field.get(clientConfig).toString()), (b) -> {
+    private static ConfigWidget<Button> createNumericSlider(WidgetData widgetData, double min, double max, double step) throws IllegalAccessException {
+        return new ConfigWidget<>(widgetData.configOption.size(), Button.builder(getWidgetText(widgetData.field.getName(), widgetData.field.get(widgetData.clientConfig).toString()), (b) -> {
 
-        }).width(configOption.size().getSize()).build());
+        }).width(widgetData.configOption.size().getSize()).build());
     }
 
-    private static ConfigWidget<EditBox> createStringInput(Field field, Class<?> type, ConfigOption configOption, ClientConfig clientConfig) throws IllegalAccessException {
-        return new ConfigWidget<>(configOption.size(), new EditBox(Minecraft.getInstance().font, configOption.size().getSize(), WIDGET_HEIGHT, getWidgetText(field.getName())));
+    private static ConfigWidget<EditBox> createStringInput(WidgetData widgetData) throws IllegalAccessException {
+        return new ConfigWidget<>(widgetData.configOption.size(), new EditBox(Minecraft.getInstance().font, widgetData.configOption.size().getSize(), WIDGET_HEIGHT, getWidgetText(widgetData.field.getName())));
+    }
+
+    private static ConfigWidget<StringWidget> createErrorWidget(ConfigOption configOption) {
+        return new ConfigWidget<>(configOption.size(), new StringWidget(configOption.size().getSize(), WIDGET_HEIGHT, Component.literal("Error"), Minecraft.getInstance().font).setColor(0xFFFF0000));
     }
 
     public static Component getWidgetText(String optionName, String optionValue) {
@@ -133,5 +140,8 @@ public class ConfigWidget<T extends AbstractWidget> {
 
     private static String getOptionValueTranslateKey(String optionName, String optionValue) {
         return String.format(OPTION_VALUE_TRANSLATE_KEY_FORMAT, optionName, optionValue);
+    }
+
+    private record WidgetData(Field field, Class<?> type, ConfigOption configOption, ClientConfig clientConfig) {
     }
 }
