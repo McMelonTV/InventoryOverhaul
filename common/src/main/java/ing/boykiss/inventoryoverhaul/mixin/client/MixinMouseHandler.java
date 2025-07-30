322bc7761883ceaa14d7b9699aa528534c8fcba4
mel <mcmelon@nodiumhosting.com>
2025-07-30 11:29:24 +0200

    fix: hotbar scrolling

--- common/src/main/java/ing/boykiss/inventoryoverhaul/mixin/client/MixinMouseHandler.java
+++ common/src/main/java/ing/boykiss/inventoryoverhaul/mixin/client/MixinMouseHandler.java
@@ -1,6 +1,5 @@
 package ing.boykiss.inventoryoverhaul.mixin.client;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import ing.boykiss.inventoryoverhaul.client.config.ClientConfig;
 import ing.boykiss.inventoryoverhaul.client.keybind.ModifierKeybind;
 import ing.boykiss.inventoryoverhaul.imixin.IMixinInventory;
@@ -9,8 +8,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.MouseHandler;
 import net.minecraft.util.Tuple;
 import net.minecraft.world.entity.player.Inventory;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -18,15 +19,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MouseHandler.class)
 public class MixinMouseHandler {
-    @Shadow
-    private double accumulatedScrollX;
-
+    @Final
     @Shadow
     private Minecraft minecraft;
 
     // start vanilla
-    private static int getNextScrollWheelSelection(double j, int k, int l) {
-        int i = (int)Math.signum(j);
+    @Unique
+    private static int inventoryoverhaul$getNextScrollWheelSelection(double j, int k, int l) {
+        int i = (int) Math.signum(j);
         k -= i;
         k = Math.max(-1, k);
 
@@ -44,7 +44,6 @@ public class MixinMouseHandler {
 
     @Inject(method = "onScroll", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;swapPaint(D)V"))
     private void onScroll(long l, double d, double e, CallbackInfo ci) {
-        int k = (int) this.accumulatedScrollX;
         Inventory inventory = this.minecraft.player.getInventory();
         boolean modifierKeyHeld = ModifierKeybind.MODIFIER_KEYMAPPING.isDown();
 
@@ -60,10 +59,11 @@ public class MixinMouseHandler {
                     ? inventory.selected
                     : hotbar.getSlotIndexColumn(inventory.selected);
             int selectionSize = Inventory.getSelectionSize();
-            int next = getNextScrollWheelSelection(k, base, selectionSize);
+            int next = inventoryoverhaul$getNextScrollWheelSelection(e, base, selectionSize);
             int slotIndex = byRow
                     ? next
                     : hotbar.getSlotIndexColumn(next);
+
             inventory.swapPaint(slotIndex);
         } else if (hotbarScrollMode == ClientConfig.HotbarScrollMode.SPLIT) {
             Tuple<Integer, Integer> currentXY = hotbar.getSlotXY(inventory.selected);
@@ -72,9 +72,9 @@ public class MixinMouseHandler {
             int nextY = currentXY.getB();
 
             if (scrollByColumn) {
-                nextY = getNextScrollWheelSelection(k, currentXY.getB(), hotbar.getSizeY());
+                nextY = inventoryoverhaul$getNextScrollWheelSelection(e, currentXY.getB(), hotbar.getSizeY());
             } else {
-                nextX = getNextScrollWheelSelection(k, currentXY.getA(), hotbar.getSizeX());
+                nextX = inventoryoverhaul$getNextScrollWheelSelection(e, currentXY.getA(), hotbar.getSizeX());
             }
 
             inventory.swapPaint(hotbar.getSlotIndex(nextX, nextY));
